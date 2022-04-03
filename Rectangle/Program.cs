f1f648977ff7f53c972d4b660ba786d517a70e66
Jared ROG <guitar0317@gmail.com>
2022-04-03 11:07:23 +0800

    加入專案檔案。

---
+++ Rectangle/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace RectangleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle(4, 3);
+            //r.Acceptdetails();
+            r.Display();
+
+        }
+    }
+
+    class Rectangle
+    {
+        // 成員變量
+        double length;
+        double width;
+        public Rectangle(double l,double w)
+        {
+            this.length = l;
+            this.width = w;
+        }
+      
+        public void Acceptdetails()
+        {
+            length = 4.5;
+            width = 3.5;
+        }
+        public double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    }
+}
